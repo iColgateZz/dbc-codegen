14841034a7526312df3d720468b2b6d4c645aeb9
pavelpavliv <ppavli@taltech.ee>
2026-03-16 22:47:34 +0200

    rm dead code

--- src/ir/signal_extended_value_type.rs
+++ src/ir/signal_extended_value_type.rs
@@ -1,4 +1,3 @@
-use can_dbc::SignalExtendedValueTypeList;
 use can_dbc::SignalExtendedValueType as ParsedExtendedValueType;
 
 #[derive(Debug, Clone)]
@@ -14,19 +13,6 @@ impl From<ParsedExtendedValueType> for ExtendedValueType {
             ParsedExtendedValueType::SignedOrUnsignedInteger => Self::Integer,
             ParsedExtendedValueType::IEEEfloat32Bit => Self::Float32,
             ParsedExtendedValueType::IEEEdouble64bit => Self::Double64,
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct SignalExtendedValueType {
-    pub extended_value_type: ExtendedValueType,
-}
-
-impl From<SignalExtendedValueTypeList> for SignalExtendedValueType {
-    fn from(value: SignalExtendedValueTypeList) -> Self {
-        Self {
-            extended_value_type: value.signal_extended_value_type.into()
         }
     }
 }
