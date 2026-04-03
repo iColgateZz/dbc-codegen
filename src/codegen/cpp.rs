0f564892501ad7a94dad0fad12c9d454b192d32b
Rasmus <rasmus.raasuke@gmail.com>
2026-04-03 17:29:44 +0300

    use function to separate code generation

--- src/codegen/cpp.rs
+++ src/codegen/cpp.rs
@@ -6,19 +6,26 @@ impl CppGen {
     pub fn generate(file: &DbcFile) -> String {
         let mut out = Generator::new();
 
+        Self::includes(&mut out);
+        Self::messages(&mut out, file);
+
+        out.into_string()
+    }
+
+    fn includes(out: &mut Generator) {
         line!(out, "#include <cstdint>");
         line!(out, "#include <cstddef>");
         line!(out, "#include <span>");
         line!(out, "#include <utility>");
         line!(out, "#include <variant>");
         empty!(out);
-
+    }
+    
+    fn messages(out: &mut Generator, file: &DbcFile) {
         for message in &file.messages {
             start_block!(out, "struct {}", message.name.0);
             end_block!(out, "");
             empty!(out);
         }
-
-        out.into_string()
     }
 }
