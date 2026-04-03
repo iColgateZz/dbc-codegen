54a22131f0ecc1cbb73b50f082776ab55519a189
Rasmus <rasmus.raasuke@gmail.com>
2026-04-03 12:40:27 +0300

    getting started with cpp code generation (using the new generator)

--- data/generated.cpp
+++ data/generated.cpp
@@ -1,1 +1,21 @@
-//TODO
+#include <cstdint>
+#include <cstddef>
+#include <span>
+#include <utility>
+#include <variant>
+
+struct DRIVER_HEARTBEAT {
+};
+
+struct IO_DEBUG {
+};
+
+struct MOTOR_CMD {
+};
+
+struct MOTOR_STATUS {
+};
+
+struct SENSOR_SONARS {
+};
+
--- src/codegen/cpp.rs
+++ src/codegen/cpp.rs
@@ -1,9 +1,24 @@
-use crate::DbcFile;
+use crate::{DbcFile, codegen::Generator, empty, end_block, line, start_block};
 
 pub struct CppGen;
 
 impl CppGen {
-    pub fn generate(_file: &DbcFile) -> String {
-        return format!("//TODO");
+    pub fn generate(file: &DbcFile) -> String {
+        let mut out = Generator::new();
+
+        line!(out, "#include <cstdint>");
+        line!(out, "#include <cstddef>");
+        line!(out, "#include <span>");
+        line!(out, "#include <utility>");
+        line!(out, "#include <variant>");
+        empty!(out);
+
+        for message in &file.messages {
+            start_block!(out, "struct {}", message.name.0);
+            end_block!(out, "");
+            empty!(out);
+        }
+
+        out.into_string()
     }
 }
