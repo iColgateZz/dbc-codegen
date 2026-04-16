331b76e8ba7abd0eded09c719ae3c99ed5dd21f8
pavelpavliv <ppavli@taltech.ee>
2026-04-16 19:03:39 +0300

    ensure message name uniqueness

--- src/middle_end/nodes/msg_name_sanitizer.rs
+++ src/middle_end/nodes/msg_name_sanitizer.rs
@@ -1,12 +1,30 @@
+use std::collections::HashMap;
+
 use super::transformation::TransformationNode;
 
-/// Append _MSG to message name
+/// Append _MSG to message name and ensure uniqueness
 pub struct SanitizeMessageNames;
 
 impl TransformationNode for SanitizeMessageNames {
     fn transform(&self, file: &mut crate::DbcFile) {
+        let mut counts: HashMap<String, usize> = HashMap::new();
+
         for msg in &mut file.messages {
-            msg.name.0 = format!("{}_MSG", msg.name.0);
+            let base = format!("{}_MSG", msg.name.0);
+
+            let count = counts.entry(base.to_lowercase()).or_insert(0);
+
+            let new_name = if *count == 0 {
+                base
+            } else {
+                format!("{}{}", base, count)
+            };
+
+            *count += 1;
+
+            msg.name.0 = new_name;
         }
+
+        println!("{counts:?}");
     }
 }
